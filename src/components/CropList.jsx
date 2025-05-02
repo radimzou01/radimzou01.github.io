8bc12519206405a7824bde837411b94ccfa87cea
radim <radim.zouharhs.hotkey.bind({"ctrl"}, "o", function()    hs.eventtap.keyStrokes("Tested")    hs.eventtap.keyStroke({}, "return" , 1000)    hs.eventtap.keyStroke({}, "return" , 1000)    hs.eventtap.keyStrokes("**Env**")    hs.eventtap.keyStroke({}, "return" , 1000)    hs.eventtap.keyStroke({}, "return" , 1000)    hs.eventtap.keyStroke({}, "return" , 1000)    hs.eventtap.keyStrokes("**Version**")    hs.eventtap.keyStroke({}, "return" , 1000)    hs.eventtap.keyStroke({}, "return" , 1000)    hs.eventtap.keyStroke({}, "return" , 1000)    hs.eventtap.keyStrokes("**Device+Browser**")    hs.eventtap.keyStroke({}, "return" , 1000)    hs.eventtap.keyStrokes("`MAC` CH, FF, EG, SF")    hs.eventtap.keyStroke({}, "return" , 1000)    hs.eventtap.keyStrokes("`WIN` CH, FF")    hs.eventtap.keyStroke({}, "return" , 1000)    hs.eventtap.keyStroke({}, "return" , 1000)    hs.eventtap.keyStrokes("**Status**")    hs.eventtap.keyStroke({}, "return" , 1000)    hs.eventtap.keyStrokes("#Works")    hs.eventtap.keyStroke({}, "return" , 1000)    hs.eventtap.keyStroke({}, "return" , 1000)    hs.eventtap.keyStrokes(":white_check_mark: ")    hs.eventtap.keyStroke({}, "return" , 1000)    hs.eventtap.keyStrokes(":white_check_mark: ")    hs.eventtap.keyStroke({}, "return" , 1000)    hs.eventtap.keyStroke({}, "return" , 1000)    hs.eventtap.keyStrokes("#Need improvements")    hs.eventtap.keyStroke({}, "return" , 1000)    hs.eventtap.keyStroke({}, "return" , 1000)    hs.eventtap.keyStrokes(":x: ")    hs.eventtap.keyStroke({}, "return" , 1000)    hs.eventtap.keyStroke({}, "return" , 1000)    hs.eventtap.keyStrokes("#For further testing")    hs.eventtap.keyStroke({}, "return" , 1000)    hs.eventtap.keyStroke({}, "return" , 1000)    hs.eventtap.keyStrokes("#To discuss")    hs.eventtap.keyStroke({}, "return" , 1000)    hs.eventtap.keyStroke({}, "return" , 1000)    hs.eventtap.keyStrokes(":question: ")    hs.eventtap.keyStroke({}, "return" , 1000)    hs.eventtap.keyStroke({}, "return" , 1000)    hs.eventtap.keyStrokes("**Note**")    hs.eventtap.keyStroke({}, "return" , 1000)    hs.eventtap.keyStroke({}, "return" , 1000)    hs.eventtap.keyStroke({}, "return" , 1000)    hs.eventtap.keyStrokes("---")    hs.eventtap.keyStroke({}, "return" , 1000)    hs.eventtap.keyStrokes("cc @matejplaskon1")end)hs.hotkey.bind({"ctrl"}, "b", function()    hs.eventtap.keyStrokes(":white_check_mark: ")end)hs.hotkey.bind({"ctrl"}, "n", function()    hs.eventtap.keyStrokes(":x: ")end)hs.hotkey.bind({"ctrl"}, "m", function()    hs.eventtap.keyStrokes(":question: ")end)hs.hotkey.bind({"ctrl"}, "a", function()    hs.eventtap.keyStrokes(" App.version")end)hs.hotkey.bind({"ctrl"}, "d", function()    hs.eventtap.keyStrokes("localStorage.debug = '*'")end)hs.hotkey.bind({"ctrl"}, "e", function()    hs.eventtap.keyStrokes("t@l.cz")end)hs.hotkey.bind({"ctrl"}, "w", function()    hs.eventtap.keyStrokes("console.log(_smartsupp.widgetVersion)")radim.zouhar@smartsupp.com>
2025-05-02 22:05:19 +0200

    localStorage usage

--- src/components/CropList.jsx
+++ src/components/CropList.jsx
@@ -1,9 +1,15 @@
 import Crop from "./Crop.jsx";
 
+const cropsStringifyName = 'crops'
+
 const CropList = ({crops, onHandleShowEditScreen, onHandleShowDeleteScreen, onGetCropId }) => {
+
+  const storedCrops = JSON.parse(localStorage.getItem(cropsStringifyName)) || crops
+  localStorage.setItem(cropsStringifyName, JSON.stringify(crops))
+
     return (
         <ul>
-        {Object.values(crops).map((crop) => (
+        {Object.values(storedCrops).map((crop) => (
           <Crop
             crop={crop}
             onHandleShowEditScreen={onHandleShowEditScreen}
